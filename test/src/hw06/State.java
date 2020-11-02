9ece541d19a0385a6ac4201af69c18167d1ef82f
binusrbinsbinusrsbin <73305835+binusrbinsbinusrsbin@users.noreply.github.com>
2020-11-02 10:36:02 -0500

    Delete State.java

--- test/src/hw06/State.java
+++
@@ -1,9 +0,0 @@
-package hw06;
-
-public interface State {
-
-	abstract public void openCloseButtonPushed();
-	abstract public void playButtonPushed();
-	abstract public void stopButtonPushed();
-
-}
